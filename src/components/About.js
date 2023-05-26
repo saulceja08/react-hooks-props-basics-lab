83242b8799543fa8a5af516092349e132c76afcc
Saul Ceja <saulceja08@gmail.com>
2023-05-26 13:41:55 -0700

    complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,17 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+  const { bio, links ,user} = props; // Destructure the props to extract bio and links
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bio && <p>{bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={links.github} linkedin={links.linkedin}/> 
     </div>
   );
 }
 
 export default About;
+
